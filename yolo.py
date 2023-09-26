add861ae7c86383bef1f11578f8e3f988839d99a
riha1432 <tjqjaejr9741@gmail.com>
2023-09-26 09:57:28 +0900

    yolo

--- yolo.py
+++ yolo.py
@@ -2,7 +2,7 @@ from ultralytics import YOLO
 import cv2
 
 model = YOLO('./yolov8_pretrained/yolov8n.pt')
-result = model.predict("https://ultralytics.com/images/zidane.jpg", save=True, conf=0.5)
+result = model.predict("https://ultralytics.com/images/zidane.jpg", save=False,conf=0.5)
 plots = result[0].plot()
 # cv2_imshow(plots)
 cv2.imshow("ad",plots)

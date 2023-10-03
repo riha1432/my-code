2029d1dd6ed2240b25ced0cafb1c69ad007e3cb2
riha1432 <tjqjaejr9741@gmail.com>
2023-10-03 10:28:23 +0900

    a

--- python/Flask.py
+++ python/Flask.py
@@ -20,6 +20,7 @@ def gen_frames():
       frame = buffer.tobytes()
       yield (b'--frame\r\n'
                      b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
 @app.route('/video_feed')
 def video_feed():
   return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')

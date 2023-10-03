384e9aa067cfc5b2480cd6fd1a189e9f24a59334
riha1432 <tjqjaejr9741@gmail.com>
2023-10-03 10:30:44 +0900

    node

--- node/rtsp.js
+++ node/rtsp.js
@@ -3,9 +3,9 @@ const Stream = require('node-rtsp-stream');
 stream = new Stream({
   name: 'foscam_stream',
   streamUrl: 'rtsp://210.99.70.120:1935/live/cctv001.stream',
-  //wsPort: 11525, // 웹소켓을 통한 스트림 서비스 포트
+  wsPort: 1935, // 웹소켓을 통한 스트림 서비스 포트
   ffmpegOptions: { // options ffmpeg flags
     '-stats': '', // an option with no neccessary value uses a blank string
-    '-force_fps': 30 // options with required values specify the value after the key, 30:blur있음
+    '-force_fps': 60 // options with required values specify the value after the key, 30:blur있음
   }
 });

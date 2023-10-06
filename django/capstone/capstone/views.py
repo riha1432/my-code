08b40d62894565ba40563d1658c23ae82c8794c2
riha1432 <tjqjaejr9741@gmail.com>
2023-10-06 12:18:25 +0900

    Revert "data"
    This reverts commit fb70cdcf1ed4380b8e592f7019750f017e441fe0.

--- django/capstone/capstone/views.py
+++ django/capstone/capstone/views.py
@@ -1,8 +1,5 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+
 
 def claude(request):
-    
-    # HTML 파일에 넘겨줄 추가 내역들 삽입하는 곳
-    # return HttpResponse("Hello, world. You're at the polls index.")
-    return render(request, 'templates/video.html')
+    return HttpResponse("Hello, world. You're at the polls index.")

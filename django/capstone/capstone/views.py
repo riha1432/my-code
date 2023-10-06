fb70cdcf1ed4380b8e592f7019750f017e441fe0
riha1432 <tjqjaejr9741@gmail.com>
2023-10-06 12:16:55 +0900

    data

--- django/capstone/capstone/views.py
+++ django/capstone/capstone/views.py
@@ -1,5 +1,8 @@
 from django.http import HttpResponse
-
+from django.shortcuts import render
 
 def claude(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    
+    # HTML 파일에 넘겨줄 추가 내역들 삽입하는 곳
+    # return HttpResponse("Hello, world. You're at the polls index.")
+    return render(request, 'templates/video.html')

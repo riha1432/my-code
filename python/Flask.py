ec822e0017d4523acf838861e08a2edbaae42469
tjqjaejr9741 <tjqjaejr9741@gmail.com>
2023-10-12 17:06:14 +0900

    Flas

--- python/Flask.py
+++ python/Flask.py
@@ -1,22 +1,24 @@
 from flask import Flask, render_template, Response
 import cv2
-from ultralytics import YOLO
+# from ultralytics import YOLO
 import time
 
-camera = cv2.VideoCapture('rtsp://210.99.70.120:1935/live/cctv001.stream')
-model = YOLO('./yolov8_pretrained/yolov8n.pt')
+# camera = cv2.VideoCapture('rtsp://210.99.70.120:1935/live/cctv001.stream')
+camera = cv2.VideoCapture('http://10.10.105.61:8090')
+# model = YOLO('./yolov8_pretrained/yolov8n.pt')
 app = Flask(__name__)
 
 def gen_frames():
   ret, frame = 0,0
   while True:
-    for i in range(3):
-        ret, frame = camera.read()
+    ret, frame = camera.read()
     if not ret:
       break
     else:
-      result = model.predict(frame, save=False, conf=0.4)
-      ret, buffer = cv2.imencode('.jpg', result[0].plot())
+      # result = model.predict(frame, save=False, conf=0.4)
+      # ret, buffer = cv2.imencode('.jpg', result[0].plot())
+      ret, buffer = cv2.imencode('.jpg', frame)
+
       frame = buffer.tobytes()
       yield (b'--frame\r\n'
                      b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
